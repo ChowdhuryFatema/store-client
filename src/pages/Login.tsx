f2ec1d7d13de33cf8cfc2bff7478b2652a3685b2
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2025-01-19 18:09:13 +0600

    Added auth in redux

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,67 @@
+import { Button } from "antd";
+import { FieldValues, useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hook";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { verifyToken } from "../utils/verifyToken";
 
 
 const Login = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            id: 'A-0001',
+            password: 'admin123'
+        }
+    });
+
+    const [login, { error }] = useLoginMutation();
+
+    // console.log("data => ", data)
+    console.log("error => ", error)
+
+    const onSubmit = async (data: FieldValues) => {
+        // console.log(data)
+       const toastId = toast.loading('Logging in')
+
+
+        try {
+            const userInfo = {
+                id: data.id,
+                password: data.password,
+            }
+
+            const res = await login(userInfo).unwrap();
+
+            const user = verifyToken(res.data.accessToken) as TUser;
+
+            dispatch(setUser({ user: user, token: res.data.accessToken }));
+            toast.success('Logged in', {id: toastId, duration: 2000})
+            navigate(`/${user.role}/dashboard`)
+
+        } catch (error) {
+            toast.error('Something went wrong', {id: toastId})
+        }
+
+    }
+
     return (
-        <div>
-            <h1>This is login page</h1>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID: </label>
+                <input type="text" id="id" {...register('id')} />
+            </div>
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input type="text" id="password" {...register('password')} />
+            </div>
+            <Button htmlType="submit">Login</Button>
+        </form>
     );
 };
 
--- test.js
+++ test.js
@@ -1,54 +1,54 @@
-const adminPaths2 = [
-    {
-        name: "Dashboard",
-        path: "/admin/dashboard",
-        element: 'ADMIN_DASHBOARD',
-    },
-    {
-        name: "User Management",
-        children: [
-            {
-                name: "Create Admin",
-                path: "/admin/create-admin",
-                element: 'CREATE_ADMIN',
-            },
-            {
-                name: "Create Faculty",
-                path: "/admin/create-faculty",
-                element: 'CREATE_FACULTY',
-            },
-            {
-                name: "Create Student",
-                path: "/admin/create-student",
-                element: 'CREATE_STUDENT',
-            },
-        ]
-    },
-]
+// const adminPaths2 = [
+//     {
+//         name: "Dashboard",
+//         path: "/admin/dashboard",
+//         element: 'ADMIN_DASHBOARD',
+//     },
+//     {
+//         name: "User Management",
+//         children: [
+//             {
+//                 name: "Create Admin",
+//                 path: "/admin/create-admin",
+//                 element: 'CREATE_ADMIN',
+//             },
+//             {
+//                 name: "Create Faculty",
+//                 path: "/admin/create-faculty",
+//                 element: 'CREATE_FACULTY',
+//             },
+//             {
+//                 name: "Create Student",
+//                 path: "/admin/create-student",
+//                 element: 'CREATE_STUDENT',
+//             },
+//         ]
+//     },
+// ]
 
 
-const newArray = adminPaths2.reduce((acc, item) => {
+// const newArray = adminPaths2.reduce((acc, item) => {
 
-    if (item.name && item.path) {
-        acc.push({
-            key: item.name,
-            label: "NAVELINK",
-        })
-    }
+//     if (item.name && item.path) {
+//         acc.push({
+//             key: item.name,
+//             label: "NAVELINK",
+//         })
+//     }
 
-    if (item.children) {
-        acc.push({
-            key: item.name,
-            label: item.name,
-            children: item.children.map(child => ({
-                key: child.name,
-                label: 'NAVELINK'
-            }))
-        })
-    }
+//     if (item.children) {
+//         acc.push({
+//             key: item.name,
+//             label: item.name,
+//             children: item.children.map(child => ({
+//                 key: child.name,
+//                 label: 'NAVELINK'
+//             }))
+//         })
+//     }
 
-    return acc;
-}, []);
+//     return acc;
+// }, []);
 
 
 
@@ -79,4 +79,6 @@ const newArray = adminPaths2.reduce((acc, item) => {
 //     return acc;
 // }, []);
 
-console.log(JSON.stringify(newArray))
+// console.log(JSON.stringify(newArray))
+
+
